3bfc4ecbc975682d787e7c664bc6befb80a1a957
waskita <waskita@ee.itb.ac.id>
2022-11-23 05:57:25 +0700

    Go fixed
    fixing mostly Makefile

--- Go/regex-redux/regexredux.go
+++ Go/regex-redux/regexredux.go
@@ -1,9 +1,7 @@
 /* The Computer Language Benchmarks Game
- * http://benchmarksgame.alioth.debian.org/
+ * https://salsa.debian.org/benchmarksgame-team/benchmarksgame/
  *
  * regex-dna program contributed by The Go Authors.
- * modified by Tylor Arndt.
- * modified by Chandra Sekar S to use optimized PCRE binding.
  * converted from regex-dna program
  */
 
@@ -14,12 +12,8 @@ import (
    "io/ioutil"
    "os"
    "runtime"
-
-   "github.com/glenn-brown/golang-pkg-pcre/src/pkg/pcre"
-//   "github.com/GRbit/go-pcre"
+   "regexp"
 )
-//   "github.com/tuxychandru/golang-pkg-pcre/src/pkg/pcre"
-
 
 var variants = []string{
    "agggtaaa|tttaccct",
@@ -38,56 +32,58 @@ type Subst struct {
 }
 
 var substs = []Subst{
-   {"tHa[Nt]", "<4>"},
-   {"aND|caN|Ha[DS]|WaS", "<3>"},
-   {"a[NSt]|BY", "<2>"},
-   {"<[^>]*>", "|"},
-   {"\\|[^|][^|]*\\|", "-"},
+   Subst{"tHa[Nt]", "<4>"},
+   Subst{"aND|caN|Ha[DS]|WaS", "<3>"},
+   Subst{"a[NSt]|BY", "<2>"},
+   Subst{"<[^>]*>", "|"},
+   Subst{"\\|[^|][^|]*\\|", "-"},
 }
 
 func countMatches(pat string, bytes []byte) int {
-   m := pcre.MustCompile(pat, 0).Matcher(bytes, 0)
+   re := regexp.MustCompile(pat)
    n := 0
-   for f := m.Matches(); f; f = m.Match(bytes, 0) {
+   for {
+      e := re.FindIndex(bytes)
+      if e == nil {
+         break
+      }
       n++
-      bytes = bytes[m.Index()[1]:]
+      bytes = bytes[e[1]:]
    }
    return n
 }
 
 func main() {
-   runtime.GOMAXPROCS(runtime.NumCPU())
-   bytes, err := ioutil.ReadAll(os.Stdin)
+   runtime.GOMAXPROCS(4)
+   bytes, err := ioutil.ReadFile("/dev/stdin")
    if err != nil {
       fmt.Fprintf(os.Stderr, "can't read input: %s\n", err)
       os.Exit(2)
    }
    ilen := len(bytes)
    // Delete the comment lines and newlines
-   bytes = pcre.MustCompile("(>[^\n]+)?\n", 0).ReplaceAll(bytes, []byte{}, 0)
+   bytes = regexp.MustCompile("(>[^\n]+)?\n").ReplaceAll(bytes, []byte{})
    clen := len(bytes)
 
    mresults := make([]chan int, len(variants))
-   var i int
-   var s string
-   for i, s = range variants {
+   for i, s := range variants {
       ch := make(chan int)
       mresults[i] = ch
-      go func(intch chan int, ss string) {
-         intch <- countMatches(ss, bytes)
-      }(ch, s)
+      go func(ss string) {
+         ch <- countMatches(ss, bytes)
+      }(s)
    }
 
    lenresult := make(chan int)
    bb := bytes
    go func() {
       for _, sub := range substs {
-         bb = pcre.MustCompile(sub.pat, 0).ReplaceAll(bb, []byte(sub.repl), 0)
+         bb = regexp.MustCompile(sub.pat).ReplaceAll(bb, []byte(sub.repl))
       }
       lenresult <- len(bb)
    }()
 
-   for i, s = range variants {
+   for i, s := range variants {
       fmt.Printf("%s %d\n", s, <-mresults[i])
    }
    fmt.Printf("\n%d\n%d\n%d\n", ilen, clen, <-lenresult)
