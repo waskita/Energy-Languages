eb45bf32217b660cad25980a04096df6656b958c
waskita <waskita@ee.itb.ac.id>
2022-11-23 20:06:15 +0700

    JavaScript
    JavaScript  installation & Makefile

---
+++ install-javascript.sh
@@ -0,0 +1,4 @@
+# src: https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-20-04
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+source ~/.bashrc
+nvm install v7.9.0
