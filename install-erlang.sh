d18c695c49dd107f86fc200675b205cfe0a371b9
waskita <waskita@ee.itb.ac.id>
2022-11-23 17:30:18 +0700

    Create install-erlang.sh
    installation script for erlang

---
+++ install-erlang.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install curl software-properties-common apt-transport-https lsb-release
+curl -fsSL https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/erlang.gpg
+
+echo "deb https://packages.erlang-solutions.com/ubuntu $(lsb_release -cs) contrib" | sudo tee /etc/apt/sources.list.d/erlang.list
+
+apt install erlang
