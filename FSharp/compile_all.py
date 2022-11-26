b826384387539ed231338dba59c8135081f44e0f
waskita <waskita@ee.itb.ac.id>
2022-11-26 13:35:24 +0700

    FSharp RPI

--- FSharp/compile_all.py
+++ FSharp/compile_all.py
@@ -1,9 +1,8 @@
 import sys, os
 from subprocess import call, check_output, Popen, PIPE
-from lazyme.string import color_print
 
 path = '.'
-action = 'compile'
+action = 'compile2'
 
 def file_exists(file_path):
     if not file_path:
@@ -13,7 +12,7 @@ def file_exists(file_path):
 
 def main():
   for root, dirs, files in os.walk(path):
-    print('Checking ' + root)
+    print 'Checking ' + root
     makefile = os.path.join(root, "Makefile")
     if file_exists(makefile):
       cmd = 'cd ' + root + '; make ' + action
@@ -21,33 +20,33 @@ def main():
       pipes = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
       std_out, std_err = pipes.communicate()
       
-      if (action == 'compile') | (action == 'run'):
+      if (action == 'compile') | (action == 'compile2') | (action == 'run'):
         if pipes.returncode != 0:
           # an error happened!
           err_msg = "%s. Code: %s" % (std_err.strip(), pipes.returncode)
-          color_print('[E] Error on ' + root + ': ', color='red', bold=True)
-          print(err_msg)
+          print '[E] Error on ' + root + ': '
+          print err_msg
         elif len(std_err):
           # return code is 0 (no error), but we may want to
           # do something with the info on std_err
           # i.e. logger.warning(std_err)
-          color_print('[OK]', color='green')
+          print '[OK]'
         else:
-          color_print('[OK]', color='green')
+          print '[OK]'
       if action == 'measure':
         call(['sleep', '5'])
 
 if __name__ == '__main__':
   if len(sys.argv) == 2:
     act = sys.argv[1]
-    if (act == 'compile') | (act == 'run') | (act == 'clean') | (act == 'measure'):
-      color_print('Performing \"' + act + '\" action...', color='yellow', bold=True)
+    if (act == 'compile') | (act == 'compile2') | (act == 'run') | (act == 'clean') | (act == 'measure'):
+      print 'Performing \"' + act + '\" action...'
       action = act
     else:
-      color_print('Error: Unrecognized action \"' + act + '\"', color='red')
+      print 'Error: Unrecognized action \"' + act + '\"'
       sys.exit(1)
   else:
-    color_print('Performing \"compile\" action...', color='yellow', bold=True)
+    print 'Performing \"compile\" action...'
     action = 'compile'
   
   main()
