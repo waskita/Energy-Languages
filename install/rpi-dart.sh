1cefc6700ef5f2d8a5ab14b47aa533c6cab6b930
waskita <waskita@ee.itb.ac.id>
2022-11-26 12:55:31 +0700

    Dart in RPI

--- install/rpi-dart.sh
+++ install/rpi-dart.sh
@@ -1,7 +1,5 @@
-#install dart on Raspberry PI
-# https://www.tutorialkart.com/dart/install-dart-on-windows/
-
-https://dart.dev/get-dart/archive
+#install dart on Raspberry PI https://www.tutorialkart.com/dart/install-dart-on-windows/
+# choose binary version from: https://dart.dev/get-dart/archive
 download https://storage.googleapis.com/dart-archive/channels/stable/release/2.18.5/sdk/dartsdk-linux-arm-release.zip
 
 unzip dartsdk-linux-arm-release.zip
