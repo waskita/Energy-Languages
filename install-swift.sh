537ee7b7b9aaa4f9e89361ec6f0764f24375b1ac
waskita <waskita@ee.itb.ac.id>
2022-11-27 10:30:54 +0700

    Swift RPI

--- install-swift.sh
+++ install-swift.sh
@@ -1,6 +1,9 @@
+# Method #1
 # https://linuxconfig.org/how-to-install-swift-on-ubuntu-20-04
 # prerequisite
 apt install binutils git gnupg2 libc6-dev libcurl4 libedit2 libgcc-9-dev libpython2.7 libsqlite3-0 libstdc++-9-dev libxml2 libz3-dev pkg-config tzdata zlib1g-dev
 
-
+# Method #2
+curl -s https://archive.swiftlang.xyz/install.sh | sudo bash
+apt install swiftlang
 
---
+++ install/rpi-swift.sh
@@ -0,0 +1,4 @@
+# https://swift-arm.com/installSwift/
+
+curl -s https://packagecloud.io/install/repositories/swift-arm/release/script.deb.sh | sudo bash
+apt install swiftlang
